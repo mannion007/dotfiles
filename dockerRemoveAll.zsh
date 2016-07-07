f6d1f32d6a84749066f88d037cc97a03396aca1c
james.mannion <james.mannion@influentialsoftware.com>
2016-07-07 18:08:29 +0100

    Updated Docker Remove all

--- dockerRemoveAll.zsh
+++ dockerRemoveAll.zsh
@@ -2,6 +2,8 @@ function dockerRemoveAll () {
 	read "feelingBrave?Seriously...This will remove all docker containers, have you really thought this through? [Y/N]"
 	if [[ "$feelingBrave" =~ ^[Yy]$ ]]
 	then
-	    print "OK"
+	    docker stop $(docker ps -a -q)
+		docker rm $(docker ps -a -q)
+		print "Done. Here's to hoping you actually wanted to do that..."
 	fi
 }
